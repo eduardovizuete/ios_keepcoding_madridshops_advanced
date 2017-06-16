a6111b1ae890b369c2538c17182de6d9c95b9e6b
TODO1\evizuete <evizuete@todo1.com>
2017-06-16 14:26:24 -0500

    * Shops implement and tests

--- domain/src/main/java/io/keepcoding/madridshops/domain/model/Shops.java
+++ domain/src/main/java/io/keepcoding/madridshops/domain/model/Shops.java
@@ -1,35 +1,57 @@
 package io.keepcoding.madridshops.domain.model;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Shops implements ShopsIterable, ShopsUpdatable {
-    @Override
-    public long size() {
-        return 0;
+
+    private List<Shop> shops;
+
+    public Shops() {
+    }
+
+    // lazy getter
+    private List<Shop> getShops(){
+        if (shops == null) {
+            shops = new LinkedList<>();
+        }
+
+        return shops;
     }
 
     @Override
-    public void add(Shop shop) {
-
+    public long size() {
+        return getShops().size();
     }
 
     @Override
     public Shop get(long index) {
-        return null;
-    }
-
-    @Override
-    public void delete(Shop shop) {
-
+        return getShops().get((int) index);
     }
 
     @Override
     public List<Shop> allShops() {
-        return null;
+        List<Shop> listCopy = new LinkedList<>();
+        for (Shop shop : getShops()) {
+            listCopy.add(shop);
+        }
+
+        return listCopy;
+    }
+
+    @Override
+    public void add(Shop shop) {
+        getShops().add(shop);
+    }
+
+    @Override
+    public void delete(Shop shop) {
+        getShops().remove(shop);
     }
 
     @Override
     public void update(Shop newShop, long index) {
-
+        getShops().set((int) index, newShop);
     }
 }
