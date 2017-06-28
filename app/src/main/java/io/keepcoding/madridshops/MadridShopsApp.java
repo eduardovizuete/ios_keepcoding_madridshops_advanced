6e7afecec598805bb53af7a2d21d67769e28f4d1
TODO1\evizuete <evizuete@todo1.com>
2017-06-28 16:17:43 -0500

    * Service ShopService, start service, stop service, run on interval service

--- app/src/main/java/io/keepcoding/madridshops/MadridShopsApp.java
+++ app/src/main/java/io/keepcoding/madridshops/MadridShopsApp.java
@@ -1,13 +1,23 @@
 package io.keepcoding.madridshops;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.squareup.picasso.Picasso;
 
+import io.keepcoding.madridshops.activities.ShopsListActivity;
 import io.keepcoding.madridshops.domain.interactors.GetAllShopsInteractor;
 import io.keepcoding.madridshops.domain.interactors.GetAllShopsInteractorCompletion;
 import io.keepcoding.madridshops.domain.interactors.GetAllShopsInteractorImpl;
@@ -17,6 +27,7 @@ import io.keepcoding.madridshops.domain.managers.network.GetAllShopsManagerImpl;
 import io.keepcoding.madridshops.domain.managers.network.ManagerErrorCompletion;
 import io.keepcoding.madridshops.domain.managers.network.NetworkManager;
 import io.keepcoding.madridshops.domain.model.Shops;
+import io.keepcoding.madridshops.services.ShopService;
 
 public class MadridShopsApp extends MultiDexApplication {
 
@@ -31,6 +42,25 @@ public class MadridShopsApp extends MultiDexApplication {
 
         //Picasso.with(getApplicationContext()).setLoggingEnabled(true);
         Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);
+
+        ShopService.startRunningService(this);
+
+        Resources resources = getResources();
+        Intent intent = new Intent(this, ShopsListActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker("Ticker")    // Set the "ticker" text which is sent to accessibility services.
+                .setSmallIcon(android.R.drawable.ic_btn_speak_now)
+                .setContentTitle("You're spyed now")
+                .setContentText("Notification Text")
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        int notificationId = 0;
+        notificationManager.notify(notificationId, notification);
     }
 
     @Override
